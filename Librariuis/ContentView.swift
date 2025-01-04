4b3ff4da0741d61ebed2c9eaafe63ac330145f96
Dan Stoian <danmihai.stoian@gmail.com>
2025-01-04 23:28:29 +0200

    Added support for drag&drop

--- Librariuis/ContentView.swift
+++ Librariuis/ContentView.swift
@@ -1,10 +1,13 @@
 import SwiftUI
 import SwiftData
+import PDFKit
+import QuickLook
 
 struct ContentView: View {
-    
     @State var tags: [Tag] = []
     
+    @State var paths: [URL] = []
+        
     var body: some View {
         NavigationSplitView(sidebar: {
             Section("Tags") {
@@ -16,11 +19,36 @@ struct ContentView: View {
                 
             }
         }, detail: {
-            Text("Select a tag or collection")
-                .font(.title3)
+            if paths.isEmpty {
+                Text("Drop your PDFs here")
+                    .font(.title)
+                Image(systemName: "document.badge.plus")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            } else {
+                List(paths, id: \.self) { path in
+                    VStack {
+                        Image(systemName: "document")
+                        Text(path.absoluteString)
+                    }
+                    .padding()
+                }
+            }
         })
-        
+        .dropDestination(for: URL.self) { paths, _ in
+            for path in paths {
+                self.paths.append(path)
+            }
+            return true
+        }
         .navigationTitle("Librarius")
+        .toolbar {
+            ToolbarItem {
+                Button("add tag", systemImage: "plus") {
+                    
+                }
+            }
+        }
     }
 }
 
