e54cf6439b890c6ba56706330929a3c4bb89a643
Dan Stoian <danmihai.stoian@gmail.com>
2025-01-05 11:05:26 +0200

    Added thumbnail generation

--- Librariuis/ContentView.swift
+++ Librariuis/ContentView.swift
@@ -1,13 +1,12 @@
 import SwiftUI
 import SwiftData
-import PDFKit
-import QuickLook
+import QuickLookThumbnailing
 
 struct ContentView: View {
     @State var tags: [Tag] = []
-    
-    @State var paths: [URL] = []
         
+    @State var pdfs: [(NSImage, URL)] = []
+    
     var body: some View {
         NavigationSplitView(sidebar: {
             Section("Tags") {
@@ -19,26 +18,28 @@ struct ContentView: View {
                 
             }
         }, detail: {
-            if paths.isEmpty {
+            if pdfs.isEmpty {
                 Text("Drop your PDFs here")
                     .font(.title)
                 Image(systemName: "document.badge.plus")
                     .resizable()
                     .frame(width: 64, height: 64)
             } else {
-                List(paths, id: \.self) { path in
+                List(pdfs, id: \.1) { pdf in
                     VStack {
-                        Image(systemName: "document")
-                        Text(path.absoluteString)
+                        Image(nsImage: pdf.0)
+                        Text(pdf.1.lastPathComponent)
                     }
                     .padding()
                 }
             }
         })
         .dropDestination(for: URL.self) { paths, _ in
+           
             for path in paths {
-                self.paths.append(path)
+                createThunmbnail(for: path, of: CGSize(width: 128, height: 128))
             }
+            
             return true
         }
         .navigationTitle("Librarius")
@@ -50,10 +51,26 @@ struct ContentView: View {
             }
         }
     }
+    
+    func createThunmbnail(for url: URL, of size: CGSize)  {
+        let request = QLThumbnailGenerator.Request(fileAt: url, size: size, scale: 1.0 as CGFloat, representationTypes: .thumbnail)
+        
+        QLThumbnailGenerator.shared.generateRepresentations(for: request) {
+            representation, type, err in
+            
+            guard err == nil else {
+                fatalError("Cannot generate thumbnail")
+            }
+            
+            if let representation {
+                pdfs.append((representation.nsImage, url))
+            }
+        }
+    }
 }
 
 
 #Preview {
     ContentView(tags: Tag.examples)
-      
+    
 }
